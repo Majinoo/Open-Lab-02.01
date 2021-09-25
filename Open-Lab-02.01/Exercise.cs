04ab2b78589ef489fa18ceeeccdcc30f5a22a0f0
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-09-25 19:54:44 +0200

    Implemented method which returns array of 5 double numbers.

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -6,12 +6,13 @@ namespace Open_Lab_02._01
     {
         public double[] Create5Numbers()
         {
-            throw new NotImplementedException();
+            double[] hodnota = { 2.1, 1.9, 5.1, 4.4, 5.5 };
+            return hodnota;
         }
 
         public double GetSecond(double[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers[1];
         }
     }
 }
